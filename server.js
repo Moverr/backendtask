7c300c5a8a163ee636962a8dc0b46fbfa7dde840
Muyinda ROgers <moverr@gmail.com>
2020-04-12 00:52:12 +0300

    WOrking upon the Upload and resize functionality :

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
-require('dotenv').config()
+const jsonpatch  = require('fast-json-patch');
 const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser');
+
+const app = express() 
 const PORT = process.env.PORT || 3300
 var authController = require('./app/controllers/authController')
-// const authenticatetoken = require('app/controllers/authController.js')
+var Resize = require('./app/middleware/Resize');
+var multer  = require('multer')
+//  var upload = multer({ dest: 'uploads/' })
+var upload = require('./app/middleware/uploadMiddleware');
 
-app.use(express.json());
-
+app.use(express.static(__dirname + 'public')); 
+app.use('/images', express.static(__dirname + '/uploads'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',authController.authenticatetoken, (req, res) => {
   res.send('Hello World')
 })
 
 
-
+//not yest implemented staff 
 app.patch('/',authController.authenticatetoken, (req, res) => {
-  res.send("Great is the lord")
+   let document  = null
+   document = jsonpatch.applyPatch(req.body['original'], req.body['patch']).newDocument; 
+   res.send(document)
 })
+app.use(express.static('/uploads'))
+const path = require('path');
+app.post('/upload',authController.authenticatetoken,upload.single('avatar'),  async   (req, res) => {
+  const imagePath = path.join(__dirname, '/uploads');
+  console.log(req.file);
+  const fileUpload = new Resize(imagePath);
+  if (!req.file) {
+    res.status(401).json({error: 'Please provide an image'});
+  }
+  const filename = await fileUpload.save(req.file.buffer);
+  return res.status(200).json({ "url":"http://localhost:"+PORT+"/images/"+filename });
+
+ 
+   
+})
+
+
 
 
 
